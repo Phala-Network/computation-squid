9a1133ed3ad4f09d7c6dfe20a09e54123c54b795
kingsleydon <10992364+kingsleydon@users.noreply.github.com>
2022-12-16 20:47:02 +0800

    pc4 reset

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ const config: {
   dataSource: DataSource
   blockRange: Exclude<BlockRangeOption['range'], undefined>
 } = {
-  blockRange: {from: 3036},
+  blockRange: {from: 7801},
   dataSource: {
     archive: 'http://51.210.116.29:8889/graphql',
     chain: 'wss://pc-test-4.phala.network/khala/ws',
